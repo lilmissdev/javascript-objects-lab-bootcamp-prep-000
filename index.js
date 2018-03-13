cc9016b1fdfaf4d180f4fad59dd033f31081899b
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-13 04:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ function deleteFromObjectByKey(object, key) {
   return newObj
 }
 
+<<<<<<< HEAD
 function destructivelyDeleteFromObjectByKey(object, key) {
+=======
+function desructivelyDeleteFromObjectByKey(object, key) {
+>>>>>>> 69465171da8381e0652f04cd7867f52dbd7fa886
   delete object[key]
   return object
 }
